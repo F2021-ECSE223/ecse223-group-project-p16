4582f7f30c148c548e5b1f461f9beee8f1791d34
yakirbender <77910849+yakirbender@users.noreply.github.com>
2021-10-14 17:33:38 -0400

    Reverting to 941a25e904523cb3d36bd8017cb8fef42b41e8f9

---
+++ ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/P16StepDefinitions.java
@@ -0,0 +1,66 @@
+package ca.mcgill.ecse.climbsafe.features;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class P16StepDefinitions {
+  @Given("the following ClimbSafe system exists: \\(p16)")
+  public void the_following_climb_safe_system_exists_p16(
+      io.cucumber.datatable.DataTable dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+    //
+    // For other transformations you can register a DataTableType.
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("the following hotels exist in the system: \\(p16)")
+  public void the_following_hotels_exist_in_the_system_p16(
+      io.cucumber.datatable.DataTable dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+    //
+    // For other transformations you can register a DataTableType.
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("the administator attempts to delete the hotel in the system with name {string} \\(p16)")
+  public void the_administator_attempts_to_delete_the_hotel_in_the_system_with_name_p16(
+      String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the number of hotels in the system shall be {string} \\(p16)")
+  public void the_number_of_hotels_in_the_system_shall_be_p16(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the hotel with name {string}, address {string}, and rating {string} shall not exist in the system \\(p16)")
+  public void the_hotel_with_name_address_and_rating_shall_not_exist_in_the_system_p16(
+      String string, String string2, String string3) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("the following hotels shall exist in the system: \\(p16)")
+  public void the_following_hotels_shall_exist_in_the_system_p16(
+      io.cucumber.datatable.DataTable dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+    //
+    // For other transformations you can register a DataTableType.
+    throw new io.cucumber.java.PendingException();
+  }
+}
