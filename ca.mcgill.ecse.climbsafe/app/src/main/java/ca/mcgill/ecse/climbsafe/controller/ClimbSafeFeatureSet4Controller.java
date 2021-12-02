57d345db55eccb9b044aa58b9d052c79a8568812
Rui Du <58449919+ruiding-code@users.noreply.github.com>
2021-12-01 23:39:12 -0500

    Equipment rui view (#65)
    * modified view for the equipment page
    
    * recuperated controller set 4

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet4Controller.java
@@ -20,7 +20,45 @@ public class ClimbSafeFeatureSet4Controller {
    * @throws InvalidInputException
    */
   public static void addEquipment(String name, int weight, int pricePerWeek)
-      throws InvalidInputException {}
+    throws InvalidInputException {
+    
+    var error = "";
+      
+    if (name.isEmpty()|| name == null) {
+      error = "The name must not be empty";
+    }
+    if (weight <= 0) {
+      error = "The weight must be greater than 0";
+    }
+    if (pricePerWeek < 0) {
+      error = "The price per week must be greater than or equal to 0";
+    }
+    BookableItem item = BookableItem.getWithName(name);
+    if (item != null) {
+      if (item instanceof Equipment) {
+        error = "The piece of equipment already exists";
+      }
+      else {
+        error = "The equipment bundle already exists";
+      }
+    }
+      
+    if (!error.isEmpty()) {
+      throw new InvalidInputException(error.trim());
+    }
+      
+    try {
+      climbSafe.addEquipment(name, weight, pricePerWeek);
+      ClimbSafePersistence.save();
+    } catch (RuntimeException e) {
+      error = e.getMessage();
+      if (error.startsWith("Cannot create due to duplicate name.")) {
+        error = "A bookable item with this name already exists. Please use a different name.";     
+      }
+      throw new InvalidInputException(error);
+    }
+      
+  }
 
   /**
    * updateEquipment:
@@ -37,6 +75,56 @@ public class ClimbSafeFeatureSet4Controller {
    * @throws InvalidInputException
    */
   public static void updateEquipment(String oldName, String newName, int newWeight,
-      int newPricePerWeek) throws InvalidInputException {}
-
+    int newPricePerWeek) throws InvalidInputException {
+      
+    var error = "";
+      
+    if (newName.isEmpty()|| newName == null) {
+      error = "The name must not be empty";
+    }
+    if (oldName.isEmpty()|| oldName == null) {
+      error = "Old equipment name cannot be empty or null.";
+    }
+    if (newWeight <= 0) {
+      error = "The weight must be greater than 0";
+    }
+    if (newPricePerWeek < 0) {
+      error = "The price per week must be greater than or equal to 0";
+    }
+     
+    BookableItem existingBookableItem = BookableItem.getWithName(oldName);
+    if (existingBookableItem == null || !(existingBookableItem instanceof Equipment)) {
+      error = "The piece of equipment does not exist";
+    }
+    
+    if (!newName.equals(oldName)) {
+      BookableItem takenName = BookableItem.getWithName(newName);
+      if (takenName != null) { // should be null if the name does not exist in the system
+        if (takenName instanceof Equipment) {
+          error = "The piece of equipment already exists";
+        } else {
+          error = "An equipment bundle with the same name already exists";
+        }
+      }
+    }
+    
+    if (!error.isEmpty()) {
+      throw new InvalidInputException(error.trim());
+    }
+    
+    try {
+      existingBookableItem.setName(newName);
+      Equipment existingEquipment = (Equipment) existingBookableItem;
+      existingEquipment.setWeight(newWeight);
+      existingEquipment.setPricePerWeek(newPricePerWeek);
+      
+      ClimbSafePersistence.save();
+    } catch (RuntimeException e) {
+      error = e.getMessage();
+      throw new InvalidInputException(error);
+    }
+    
+      
+  }
+  
 }
